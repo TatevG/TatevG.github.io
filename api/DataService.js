6ca3a93e4ef57ec093d7e482a1188665ca584f81
Tatevik <tatevikfvardanyan@gmail.com>
2022-04-05 17:42:48 +0400

    added dataservice and code cleanup

---
+++ api/DataService.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+const API_KEY = '69636f29401aff20cfa640a04a91ff58';
+
+axios.defaults.headers.post = {
+	'Content-Type': 'application/json'
+};
+
+export const SearchAddressByQuery = async query => {
+	let response = await axios.get(`https://photon.komoot.io/api/?q=${query}&osm_tag=place:city`);
+	return response.data;
+};
+
+export const GetWeatherByCoordinates = async (lat, lon) => {
+	let response = axios.get(`http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&exclude=hourly,minutely,alerts&units=metric&appid=${API_KEY}`)
+	return response;
+};
+
+export const GetLocationLatLong = async (location) => {
+	const response = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=${API_KEY}`);
+	return response;
+};
+
+export const GetFutureDaysForecast = async (lat, lon) => {
+	const response = await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely,alerts&units=metric&appid=${API_KEY}`);
+	return response;
+};
+
+export const GetPastDaysData = async (lat, lon, day) => {
+	const response = await axios.get(`https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=${lat}&lon=${lon}&units=metric&dt=${day}&appid=${API_KEY}`);
+	return response;
+};
--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <v-container fill-height>
 
     <v-row class="mx-sm-16 mx-4 my-2 fill-height">
-      <v-col cols="12" class="text-center" v-if="!locationData">
+      <v-col cols="12" class="text-center" v-if="pageLoading">
         <v-progress-circular
           indeterminate
           class="page-loading"
@@ -13,24 +13,37 @@
 
       <v-col cols="12" v-else>
         <v-row class="align-baseline mx-auto">
-          <!-- <v-text-field
-            v-model="location"
-            outlined
-            label="Type location name here"
-            append-icon="mdi-map-marker"
-          ></v-text-field> -->
           <v-autocomplete
-            v-model="location"
-            :items="suggestedCities"
-            outlined
-            item-text="locality"
-            item-value="locality"
-            auto-select-first
-            label="Type location name here"
-            append-icon="mdi-map-marker"
-            @change="getSyggestedCities(item)"
-          ></v-autocomplete>
-          <v-btn @click="getLocationLatLong" class="ml-2 primary">search</v-btn>
+              v-model="location"
+              :items="searchResults"
+              outlined
+              label="Type location name here"
+              append-icon="mdi-map-marker"
+              :search-input.sync="query"
+              hide-details
+              item-value="name"
+              item-text="name"
+          >
+            <template #no-data>
+                <div>
+                  No data found
+                </div>
+            </template>
+
+            <template #item="{ item, on, attrs }">
+                <v-list-item
+                    two-line
+                    v-bind="attrs"
+                    v-on="on"
+                >
+                    <v-list-item-content>
+                        <v-list-item-title>
+                            {{ item.name }}, {{ item.country }}
+                        </v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </template>
+        </v-autocomplete>
         </v-row>
 
         <v-row class="justify-space-between flex-column flex-md-row">
@@ -115,54 +128,55 @@
             mode="out-in"
             name="slide-fade"
         >
-        <v-card v-if="showAdditionalInfo && weatherData" class="weather-main-data-container d-flex flex-column mx-auto mt-4 mb-8" max-width="650" elevation="3">
-          <h3 class="font-weight-medium mx-4 mt-4">Additional info</h3>
-          <v-row class="mx-4 flex-column flex-md-row">
-            <v-col>
-              <v-row class="justify-space-between my-4 mx-0">
-                <span>Wind</span>
+          <v-card v-if="showAdditionalInfo && weatherData" class="weather-main-data-container d-flex flex-column mx-auto mt-4 mb-8" max-width="650" elevation="3">
+            <h3 class="font-weight-medium mx-4 mt-4">Additional info</h3>
+            <v-row class="mx-4 flex-column flex-md-row">
+              <v-col>
+                <v-row class="justify-space-between my-4 mx-0">
+                  <span>Wind</span>
 
-                <span>{{ weatherData.wind_speed }}</span>
-              </v-row>
+                  <span>{{ weatherData.wind_speed }}</span>
+                </v-row>
 
-              <v-divider></v-divider>
+                <v-divider></v-divider>
 
-              <v-row class="justify-space-between my-4 mx-0">
-                <span>Humidity</span>
+                <v-row class="justify-space-between my-4 mx-0">
+                  <span>Humidity</span>
 
-                <span>{{ weatherData.humidity }}</span>
-              </v-row>
+                  <span>{{ weatherData.humidity }}</span>
+                </v-row>
 
-              <v-divider></v-divider>
+                <v-divider></v-divider>
 
-              <v-row class="justify-space-between my-4 mx-0">
-                <span>Pressure</span>
+                <v-row class="justify-space-between my-4 mx-0">
+                  <span>Pressure</span>
 
-                <span>{{ weatherData.pressure }}</span>
-              </v-row>
+                  <span>{{ weatherData.pressure }}</span>
+                </v-row>
 
-              <v-divider></v-divider>
+                <v-divider></v-divider>
 
-              <v-row class="justify-space-between my-4 mx-0">
-                <span>Sunrise</span>
+                <v-row class="justify-space-between my-4 mx-0">
+                  <span>Sunrise</span>
 
-                <span>{{ getDate(weatherData.sunrise) }}</span>
-              </v-row>
+                  <span>{{ getDate(weatherData.sunrise) }}</span>
+                </v-row>
 
-              <v-divider></v-divider>
+                <v-divider></v-divider>
 
-              <v-row class="justify-space-between my-4 mx-0">
-                <span>Sunset</span>
+                <v-row class="justify-space-between my-4 mx-0">
+                  <span>Sunset</span>
 
-                <span>{{ getDate(weatherData.sunset) }}</span>
-              </v-row>
-            </v-col>
-          </v-row>
-        </v-card>
+                  <span>{{ getDate(weatherData.sunset) }}</span>
+                </v-row>
+              </v-col>
+            </v-row>
+          </v-card>
         </transition>
 
         <v-card v-if="weatherData" class="d-flex flex-column justify-space-between days-forecast-list list-complete-item my-4 px-4">
         <v-card-title>Future 7 days forecast</v-card-title>
+
         <v-card-text class="d-flex justify-space-between flex-column flex-md-row">
           <Day
             v-for="day in futureDaysForecast"
@@ -173,14 +187,9 @@
           />
         </v-card-text>
         </v-card>
-        <v-lazy
-          v-model="isActive"
-          :options="{
-            threshold: 1
-          }"
-          min-height="200"
-          transition="fade-transition"
-        >
+
+{{ pastDaysForecast.length }}
+
         <v-card v-if="weatherData" class="d-flex flex-column justify-space-between days-forecast-list list-complete-item my-4 px-4">
         <v-card-title>Past 5 days Forecast</v-card-title>
         <v-card-text class="d-flex justify-space-between flex-column flex-md-row ">
@@ -192,7 +201,6 @@
             :icon="day.weather[day.weather.length - 1].icon"/>
         </v-card-text>
         </v-card>
-        </v-lazy>
         <NotFound v-if="showNotFound" class="mt-8"/>
       </v-col>
     </v-row>
@@ -204,10 +212,14 @@ import Day from "~/components/common/Day";
 import NotFound from '~/components/common/NotFound';
 import axios from 'axios';
 import moment from 'moment';
-import { gmapApi } from 'vue2-google-maps'
-
-
-const API_KEY = '69636f29401aff20cfa640a04a91ff58';
+import { gmapApi } from 'vue2-google-maps';
+import {
+  SearchAddressByQuery,
+  GetWeatherByCoordinates,
+  GetLocationLatLong,
+  GetFutureDaysForecast,
+  GetPastDaysData
+} from '../api/DataService';
 
 export default {
   components: {
@@ -216,8 +228,7 @@ export default {
   },
   data() {
     return {
-isActive: false,
-      loading: false,
+      pageLoading: false,
       location: '',
       showAdditionalInfo: false,
       showNotFound: false,
@@ -227,8 +238,9 @@ isActive: false,
       pastDaysForecast: [],
       units: 'metric',
       currentDate: null,
-      suggestedCities: [],
-      markers: []
+      markers: [],
+      query: '',
+      searchResults: []
     };
   },
 
@@ -236,50 +248,51 @@ isActive: false,
     google: gmapApi,
   },
 
-  mounted() {
-    this.getCurrentLocation();
+  async mounted() {
+    await this.setCurrentLocationWeather();
   },
   watch: {
-    location(newVal) {
-      if(newVal.length >= 3) {
-        console.log(1111);
-        this.getSyggestedCities(newVal);
-      }
-    }
+    location() {
+      this.getLocationLatLong()
+    },
+    query() {
+        this.searchAddressByQuery();
+    },
   },
   methods: {
-    async getSyggestedCities(city) {
-      this.suggestedCities = [];
-        const resp = await axios.get(`https://app.geocodeapi.io/api/v1/autocomplete?text=${city}&apikey=1c6244a0-b378-11ec-9373-bd4da74d97c8`);
-        resp.data.features.map(item => {
-          this.suggestedCities.push({ id: item.properties.id, locality: item.properties.locality })
-        })
-        console.log(this.suggestedCities);
-      },
-    getCurrentLocation() {
-      if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition((position) => {
-              if(position.coords.latitude && position.coords.longitude) {
-                axios.get(`http://api.openweathermap.org/geo/1.0/reverse?lat=${position.coords.latitude}&lon=${position.coords.longitude}&exclude=hourly,minutely,alerts&units=metric&appid=${API_KEY}`)
-                .then(response => {
-                  this.locationData = response.data[0];
-                  this.location = response.data[0].name;
-                  this.getFutureDaysForecast(position.coords.latitude, position.coords.longitude);
-                  this.getPastDaysData(position.coords.latitude, position.coords.longitude);
-                });
-              }
-          }, (error) => {
-              console.log(error);
+    async searchAddressByQuery() {
+        if(!this.query) return;
+        try {
+          const searchResults = await SearchAddressByQuery(this.query);
+          this.searchResults = searchResults.features.map(element => {
+            return { name: element.properties.name, country: element.properties.country }
           });
-      } else {
-          console.log('no location');
+        } catch (error) {
+          console.log(error);
+        }
+    },
+    async setCurrentLocationWeather() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(async (position) => {
+          if(position.coords.latitude && position.coords.longitude) {
+            this.pageLoading = true
+            const response = await GetWeatherByCoordinates(position.coords.latitude, position.coords.longitude);
+            if(!response) return;
+            this.locationData = response.data[0];
+            this.location = response.data[0].name;
+            this.searchResults.push({ name: response.data[0].name, country: response.data[0].country })
+            this.getFutureDaysForecast(position.coords.latitude, position.coords.longitude);
+            this.getPastDaysData(position.coords.latitude, position.coords.longitude);
+            this.pageLoading = false
+          }
+        });
       }
     },
 
     async getLocationLatLong() {
       this.showNotFound = false;
       try {
-        const response = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${this.location}&limit=5&appid=${API_KEY}`);
+        const response = await GetLocationLatLong(this.location);
         this.locationData = response.data[0];
         this.getFutureDaysForecast(this.locationData.lat, this.locationData.lon);
         this.getPastDaysData(this.locationData.lat, this.locationData.lon);
@@ -294,7 +307,7 @@ isActive: false,
 
     async getFutureDaysForecast(lat, lon) {
       try {
-        const response = await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely,alerts&units=metric&appid=${API_KEY}`);
+        const response = await GetFutureDaysForecast(lat, lon)
         this.weatherData = response.data.daily[0];
         const forecastList = response.data.daily.splice(0, 1);
         this.futureDaysForecast = response.data.daily;
@@ -305,12 +318,10 @@ isActive: false,
 },
     async getPastDaysData(lat, lon) {
       this.pastDaysForecast = [];
-      let promises = []
       for (let i = 0; i < 5; i++) {
         let day = moment().subtract(i, 'd').unix()
-        // const url = axios.get(`https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=${lat}&lon=${lon}&units=${this.units}&dt=${day}&appid=${API_KEY}`)
         try {
-          const response = await axios.get(`https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=${lat}&lon=${lon}&units=${this.units}&dt=${day}&appid=${API_KEY}`);
+          const response = await GetPastDaysData(lat, lon, day);
           this.pastDaysForecast.push(response.data.current)
         } catch (error) {
           this.locationData = null;
