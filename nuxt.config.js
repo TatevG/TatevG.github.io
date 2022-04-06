e8a45f71558c24f67c0d9d23874fa6859c63b8af
Tatevik <tatevikfvardanyan@gmail.com>
2022-04-06 16:51:32 +0400

    changes for deployment

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,6 +6,9 @@ export default {
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  router: {
+    base: '/weather-app/'
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
